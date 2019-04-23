08cf7c8d05c304fd1b3c405decaadc1d0a35989a
Uppernate <uppernatearts@gmail.com>
2019-04-24 00:00:01 +0100

    Added comments

--- UIManager.js
+++ UIManager.js
@@ -4,13 +4,16 @@ class UIManager {
         this.units = [];
     }
     make() {
+        // Create the Nine Slice objects for the Unit section and the Actions section
         this.window = game.scene.keys.default.add.nineslice(0, 100, 100, 100, 'ui-window', 4, 4);
         this.unitWindow = game.scene.keys.default.add.nineslice(0, 100, 100, 100, 'ui-unit-window', [0, 4, 9, 27]);
+        // Set correct depth and make slightly transparent
         this.window.depth = depth.get('ui', 0);
         this.window.alpha = 0.5;
         this.unitWindow.depth = depth.get('ui', 0);
         this.unitWindow.alpha = 0.5;
 
+        // ALso listen to scene's events when they happen for UI Manager's other functions
         game.scene.keys.default.event.on('update', this.update, this);
         game.scene.keys.default.event.on('resize', this.reposition, this);
     }
@@ -19,22 +22,31 @@ class UIManager {
         this.reposition();
     }
     reposition() {
+        // Get scene's camera center and game size
         let scene = game.scene.keys.default;
         const size = new Vector2(scene.windowsize);
         const pos = new Vector2(scene.camerafocus);
+        // Round just in case
         pos.x = Math.round(pos.x);
         pos.y = Math.round(pos.y);
+        // Percentages that look the best, I think
         size.x *= 0.75;
         size.y *= 0.4;
         size.y -= scene.windowsize.y / 8;
 
+        // This is how you change the size of Nine Sliced objetcs
         this.window.resize(size.x, size.y);
         this.unitWindow.resize(scene.windowsize.x - size.x, size.y);
 
+        // Note: The top left is the origin of nine sliced objects
         this.window.x = pos.x - scene.windowsize.x / 4;
         this.window.y = pos.y + scene.windowsize.y * 0.22;
+
         this.unitWindow.x = pos.x - scene.windowsize.x / 2;
         this.unitWindow.y = pos.y + scene.windowsize.y * 0.22;
+
+        // Character icons also need repositioning
+
         this.units.forEach(function (unit) {
             unit.icon.x = pos.x - scene.windowsize.x * 0.5 + scene.windowsize.x * 0.125;
             unit.icon.y = pos.y + scene.windowsize.y * 0.22 + scene.windowsize.y * 0.1;
